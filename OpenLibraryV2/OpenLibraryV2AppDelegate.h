c1c4b3c9a2800d735bee1866ec0aae72fa86e980
Liam Kaufman <liam.kaufman@gmail.com>
2011-09-27 16:45:37 -0400

    Initial Commit

---
+++ OpenLibraryV2/OpenLibraryV2AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  OpenLibraryV2AppDelegate.h
+//  OpenLibraryV2
+//
+//  Created by Liam Kaufman Simpkins on 11-09-27.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenLibraryV2AppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ OpenLibraryV2/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  OpenLibraryV2
+//
+//  Created by Liam Kaufman Simpkins on 11-09-27.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
